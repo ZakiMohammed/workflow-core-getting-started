1e5cbfd021196b28a761dc51977406c963c1a094
ZakiMohammed <shaikhzaki67@gmail.com>
2022-04-04 16:33:48 +0530

    Removed base amoung prop

--- Workflows/ProcessPayment/Steps/Initialize.cs
+++ Workflows/ProcessPayment/Steps/Initialize.cs
@@ -1,7 +1,5 @@
 public class Initialize : StepBody
 {
-    public double BaseAmount { get; set; }
-
     public override ExecutionResult Run(IStepExecutionContext context)
     {
         Console.WriteLine("Initialize");
